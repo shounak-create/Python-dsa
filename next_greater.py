878bdf9064b72a3ee7f6d0134f88b3a04f3bed25
Shounak Pandit <68001165+shounak-create@users.noreply.github.com>
2026-06-14 10:42:42 +0530

    changes in next greater

--- next_greater.py
+++ next_greater.py
@@ -1,14 +1,19 @@
-def greater(arr, ind):
-    for i in range(ind + 1, len(arr)):
-        if arr[ind] < arr[i]:
-            return arr[i]
-    return -1
+def next_greater(arr):
+    stack = []
+    ans = []
+
+    for i in range(len(arr)-1, -1, -1):
+        while stack and stack[-1] <= arr[i]:
+            stack.pop()
+        if not stack:
+            ans.append(-1)
+        else:
+            ans.append(stack[-1])
+        stack.append(arr[i])
+    ans.reverse()
+
+    return ans
 
 
 arr = [4, 5, 2, 10, 8]
-ans = []
-
-for i in range(len(arr)):
-    ans.append(greater(arr, i))
-
-print(ans)
+print(next_greater(arr))
