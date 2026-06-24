f63cacbc0df6d51e4bf80451e6ae299bfd68ab2d
Shounak Pandit <68001165+shounak-create@users.noreply.github.com>
2026-06-24 15:11:46 +0530

    Binary tree implemented

--- Binary-tree.py
+++ Binary-tree.py
@@ -1,14 +1,39 @@
-class Tree:
+class Node:
     def __init__(self,data):
         self.data = data
         self.left = None
         self.right = None
 
-class Binarytree:
-    def __init__(self):
-        self.root = None
 
-    def append_data(self,data):
-        new_node = Tree(data)
-        if self.head == None:
-            self.head = new_node
+class BinaryTree:
+    def insert_data(self,root,key):
+
+        if root is None:
+            return Node(key)
+
+        if key < root.data:
+            root.left = self.insert_data(root.left,key)
+
+        else:
+            root.right = self.insert_data(root.right,key)
+
+        return root
+
+
+    def inorder(self,root):
+        if root:
+            self.inorder(root.left)
+            print(root.data,end=" ")
+            self.inorder(root.right)
+
+
+root = None
+
+nums = [50,30,70,20,40,60,80]
+
+l1 = BinaryTree()
+
+for i in nums:
+    root = l1.insert_data(root,i)
+
+l1.inorder(root)
