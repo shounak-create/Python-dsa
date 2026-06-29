cbeac97ac21caeae74b735ab100c4a94331b21ec
Shounak Pandit <68001165+shounak-create@users.noreply.github.com>
2026-06-29 12:55:21 +0530

    changes in the binary-tree.py

--- Binary-tree.py
+++ Binary-tree.py
@@ -19,12 +19,42 @@ class BinaryTree:
 
         return root
 
+    def search(self,root,key):
+        if root:
+            if root.data == key:
+                return True
+            elif key < root.data:
+                return self.search(root.left,key)
+
+            elif key > root.data:
+                return self.search(root.right,key)
+
+            return False
+
 
     def inorder(self,root):
         if root:
             self.inorder(root.left)
             print(root.data,end=" ")
             self.inorder(root.right)
+
+    def height(self,root):
+        if root == None:
+            return 0
+
+        leftht = self.height(root.left)
+        rightht = self.height(root.right)
+
+        return max(leftht,rightht)+1
+
+    def count_nodes(self,root):
+        if root == None:
+            return 0
+
+        leftnd=self.count_nodes(root.left)
+        rightnd=self.count_nodes(root.right)
+
+        return leftnd+rightnd+1
 
 
 root = None
@@ -37,3 +67,6 @@ for i in nums:
     root = l1.insert_data(root,i)
 
 l1.inorder(root)
+print(l1.search(root,60))
+print("\nheight",l1.height(root))
+print("\ncount_nodes",l1.count_nodes(root))
